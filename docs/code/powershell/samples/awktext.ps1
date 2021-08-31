f724c1ac14826f82762c4c6637e91b4e8983c1b7
scabon <sebastien.cabon@gmail.com>
2021-08-31 23:05:04 +0200

    Add description of oneliner

--- docs/code/powershell/samples/awktext.ps1
+++ docs/code/powershell/samples/awktext.ps1
@@ -19,3 +19,16 @@ Write-Host "Sum: $Sum"
 
 # As a one-liner
 Get-Content -Path 'awktext.txt' | Measure-Object -Sum { ($_ -Split '\s' )[1] }
+
+<# Explanations
+    Get-Content -Path 'awktext.txt'
+    -> reads the file content as an array of strings (one for each line in the file)
+    Each line is passed down the pipeline, so the rest of the command (at the right of the pipe) is called for each line
+    Measure-Object -Sum { ($_ -Split '\s' )[1] }
+    -> Sums over each call (for each line) the result of the following expression: ($_ -Split '\s' )[1]
+    Now if we break it down:
+        . $_ is the current value from the pipeline (so the current line from the file)
+        . -Split take $_ (the current line), and returns an array of the values separated by '\s'
+        . '\s' matches any whitespace character, so this should be more flexible on the file format
+        . [1] takes the second value from the array returned by the split (ie the second column)
+#>
