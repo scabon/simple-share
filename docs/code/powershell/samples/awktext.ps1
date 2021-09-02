aa9d5bec7771fc9de7686e3ac109e71fe6442b3c
scabon <sebastien.cabon@gmail.com>
2021-09-02 10:33:31 +0200

    WIP: updates on bahs to pwsh examples

--- docs/code/powershell/samples/awktext.ps1
+++ docs/code/powershell/samples/awktext.ps1
@@ -1,6 +1,80 @@
 <#
     Inspired by Jim SALTER
     See: https://arstechnica.com/gadgets/2021/08/linux-bsd-command-line-101-using-awk-sed-and-grep-in-the-terminal
+    This is an exercice to translate Jim's examples of bash commands to their powershell equivalent.
+    This was done using PowerShell Core on an Ubuntu (WSL2) for easier comparison.
+    Beware: some examples might be wrong and are probably non-optimal.
+#>
+
+<# GREP examples #>
+
+# bash: netstat -anp | head -n5
+# Translates to:
+netstat -anp | Select-Object -First 5
+# Note: Select-Object can be replaced by its alias: 'select'
+
+# bash: netstat -anp | wc -l
+# Translates to:
+netstat -anp | Measure-Object -Word
+# Note: Measure-Object can be replaced by its alias: 'measure'
+
+# bash: netstat -anp | grep apache
+# Translates to:
+netstat -anp | Select-String apache
+
+#bash: netstat -anp | head -n2 ; netstat -anp | grep apache
+# Translates to:
+netstat -anp | Select-Object -First 2 ; netstat -anp | Select-String apache
+
+<# SED examples #>
+
+# bash: echo "I love my dog, dogs are great!"
+# Translates to:
+Write-Output "I love my dog, dogs are great!"
+# Note: Select-Object can be replaced by its alias: 'echo'
+
+# bash: echo "I love my dog, dogs are great!" | sed 's/dog/snake/'
+# By default, the -replace operator replaces all occurences
+# The only way I found to replace only the first one is from here:
+# https://www.reddit.com/r/PowerShell/comments/bs5tms/replace_only_first_occurrence_of_string/
+# It will make the next one easier (replace all occurences), but this one trickier
+# So it translates to:
+"I love my dog, dogs are great!" -replace '(.*?)(dog)(.*)', '$1snake$3'
+
+# bash: echo "I love my dog, dogs are great!" | sed 's/dog/snake/g'
+# Translates to:
+"I love my dog, dogs are great!" -replace 'dog', 'snake'
+
+# bash: sudo netstat -anp | grep ::80
+# Translates to:
+sudo netstat -anp | Select-String ::80
+
+# bash: sudo netstat -anp | grep ::80 | sed 's/.*LISTEN *//'
+# Translates to:
+(sudo netstat -anp | Select-String ::80) -replace '.*LISTEN *', ''
+
+<# AWK examples #>
+
+# bash: sudo netstat -anp | grep apache
+# Translates to:
+sudo netstat -anp | Select-String apache
+# Note: Select-String can be replaced by its alias: 'select'
+
+# bash: sudo netstat -anp | grep apache | awk '{print $4}'
+# Translates to:
+((sudo netstat -anp | Select-String apache -raw) -replace '\s+', ' ' -split ' ')[3]
+# For reasons, the direct -split does not work (because of the whitespaces returned by the netstat command?)
+# So we replace any number of white space characters with only one, then split the result with ' '
+# This allows to retrieve the 4 th column (id #3 in the list of items)
+
+# bash: sudo netstat -anp | grep apache | awk '{print $4, $7}
+# Translates to:
+$str = (sudo netstat -anp | Select-String apache -raw) -replace '\s+', ' ' -split ' ' ; $str[3] + " " + $str[6]
+# Not a real one-liner, but well....
+
+#TODO: not sure the examples above work with more than one line returned by netstat....
+
+<#
     Sample content of the awktext.txt file:
     1 2 3
     4 5 6
