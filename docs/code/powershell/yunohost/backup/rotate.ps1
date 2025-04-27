29e302040b48ffb60c37aa813337bdf9e9290012
Sébastien CABON <31108225+scabon@users.noreply.github.com>
2025-04-27 21:55:37 +0200

    YuNoHost backups rotate: add logs

--- docs/code/powershell/yunohost/backup/rotate.ps1
+++ docs/code/powershell/yunohost/backup/rotate.ps1
@@ -10,31 +10,38 @@ param (
     [string]$BackupFolder = '/backups/yunohost/'
 )
 
+Function Write-Timestamped {
+    param (
+        [string]$Message
+    )
+    Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss'): PS - $Message"
+}
+
 if ( -Not (Test-Path -Path $BackupFolder) ) {
     Write-Error "Backup folder $BackupFolder does not exist, or access denied"
     exit 1
 }
 
-Write-Host "Listing files in $BackupFolder"
+Write-Timestamped "Listing files in $BackupFolder"
 [System.IO.FileInfo[]]$BackUpFiles = Get-ChildItem -Path $BackupFolder
 [int]$NbBackUpFiles = $BackUpFiles.Count
 [int]$NbBackups = $NbBackUpFiles / 2
-Write-Host "Found $NbBackups backups"
-Write-Host "Want to keep $BackupsToKeep backups"
+Write-Timestamped "Found $NbBackups backups"
+Write-Timestamped "Want to keep $BackupsToKeep backups"
 # There are two files for each backup (one .tar.gz and one .info.json)
 [int]$NbBackupFilesToKeep = $BackupsToKeep * 2
 if ( $NbBackUpFiles -lt $NbBackupFilesToKeep) {
-    Write-Host "No need to rotate backup files"
+    Write-Timestamped "No need to rotate backup files"
     exit 0
 }
 [int]$NbFilesToDelete = $NbBackUpFiles - $NbBackupFilesToKeep
 [int]$NbBackupsToDelete = $NbFilesToDelete / 2
-Write-Host "Number of backups to delete: $NbBackupsToDelete"
+Write-Timestamped "Number of backups to delete: $NbBackupsToDelete"
 $BackUpFiles `
 | Sort-Object -Property LastWriteTime `
 | Select-Object -First $NbFilesToDelete `
-| ForEach-Object { Write-Host "Deleting $PSItem.FullName" }
+| ForEach-Object { Write-Timestamped "Deleting $PSItem.FullName" }
 | Remove-Item -Force
 
-Write-Host "Done"
+Write-Timestamped "Done"
 exit 0
--- docs/code/powershell/yunohost/backup/rotate.sh
+++ docs/code/powershell/yunohost/backup/rotate.sh
@@ -2,5 +2,9 @@
 # Rotates the local backups of a YuNoHost server.
 # It requires the following parameters:
 # - BackupFolder: The folder containing the backups.
-# - MaxBackups: The maximum number of backups to keep.
-pwsh -File ./rotate.ps1 -BackupFolder '/backups/yunohost' -MaxBackups 10
+# - BackupsToKeep: The maximum number of backups to keep.
+echo "$(date '+%Y-%m-%d %H:%M:%S'): SH ---------------------" >> ./rotate.log
+echo "$(date '+%Y-%m-%d %H:%M:%S'): SH - Launching rotate.sh" >> ./rotate.log
+pwsh -File ./rotate.ps1 -BackupFolder '/backups/yunohost' -BackupsToKeep 10 >> ./rotate.log
+echo "$(date '+%Y-%m-%d %H:%M:%S'): SH - Finished rotate.sh" >> ./rotate.log
+echo "$(date '+%Y-%m-%d %H:%M:%S'): SH --------------------" >> ./rotate.log
